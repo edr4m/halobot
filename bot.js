07e595b17f0e36d5362fcf16e6686afaac52e275
Aryan Jha <aryanjha@live.com>
2016-11-30 17:20:59 -0800

    Refactored ship code into modular
    Now uses a much more efficient plugin module rather than being hardcoded
    in

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ var jaredInfo = {
   avatar: '45e8fee6889688cd3547cb6216adb69c',
   bot: false }
 var responseObjects = {};
+var shipObjects = {};
 
 // message triggerer
 
@@ -33,34 +34,11 @@ bot.on("message", msg => {
     shipArray = msg.content.split(" x ");
     ship1 = shipArray[0];
     ship2 = shipArray[1];
-    if (ship1.toLowerCase() == "marlene" && ship2.toLowerCase() == "eric") {
-      msg.channel.sendMessage("erilene :smirk:");
-    } else if (ship1.toLowerCase() == "eric" && ship2.toLowerCase() == "marlene") {
-      msg.channel.sendMessage("erilene <3");
-    } else if (ship1.toLowerCase() == "yuchen" && ship2.toLowerCase() == "christine") {
-      msg.channel.sendMessage("yustine :carrot:");
-    } else if (ship1.toLowerCase() == "christine" && ship2.toLowerCase() == "yuchen") {
-      msg.channel.sendMessage("cake :cake:");
-    } else if (ship1.toLowerCase() == "carmen" && ship2.toLowerCase() == "marcus") {
-      msg.channel.sendMessage("carcus :unicorn:");
-    } else if (ship1.toLowerCase() == "anish" && ship2.toLowerCase() == "marlene") {
-      msg.channel.sendMessage("impossible!");
-    } else if (ship1.toLowerCase() == "gwen" && ship2.toLowerCase() == "carp") {
-      msg.channel.sendMessage("gwarpekl :3");
-    } else if (ship1.toLowerCase() == "gwen" && ship2.toLowerCase() == "carpekl") {
-      msg.channel.sendMessage("gwarpekl :3");
-    } else if (ship1.toLowerCase() == "emma" && ship2.toLowerCase() == "jordan") {
-      msg.channel.sendMessage("mr and mrs snails");
-    } else if (ship1.toLowerCase() == "jordan" && ship2.toLowerCase() == "emma") {
-      msg.channel.sendMessage("mr and mrs snails :heartpulse:");
-    } else if (ship1.toLowerCase() == "ruby" && ship2.toLowerCase() == "sapphire") {
-      msg.channel.sendMessage("garnet");
-    } else if (ship1.toLowerCase() == "rose" && ship2.toLowerCase() == "greg") {
-      msg.channel.sendMessage("steven");
-    } else if (ship1.toLowerCase() == "amethyst" && ship2.toLowerCase() == "pearl") {
-      msg.channel.sendMessage("and steven! :D");
-    } else if (ship1.toLowerCase() == "greg" && ship2.toLowerCase() == "van") {
-      msg.channel.sendMessage("wtf?");
+
+    if (shipObjects[ship1]) {
+      if (shipObjects[ship1][ship2]) {
+        msg.channel.sendMessage(shipObjects[ship1][ship2]);
+      }
     } else {
       ship1half = "";
       ship2half = "";
@@ -74,7 +52,7 @@ bot.on("message", msg => {
       msg.channel.sendMessage(finalShip);
     }
   }
-})
+});
 
 ;
 
@@ -104,17 +82,6 @@ bot.on('message', msg => {
   }
 });
 
-// data
-
-bot.on('message', msg => {
-  if (msg.content.match("data return channel") != null) {
-    msg.channel.sendMessage(msg.channel);
-  } else if (msg.content.match("data return user") != null) {
-    msg.channel.sendMessage(msg.author);
-  }
-});
-
-
 // functions
 
 function loadConfiguration(callback) {
@@ -131,6 +98,10 @@ function loadConfiguration(callback) {
     for (i = 0; i < fs.readdirSync('plugins/triggers').length; i++) {
       responseObjects[fs.readdirSync('plugins/triggers')[i]] = JSON.parse(fs.readFileSync('./plugins/triggers/' + fs.readdirSync('plugins/triggers')[i]))
     }
+  }
+
+  if (fs.readdirSync('plugins') != undefined || fs.readdirSync('plugins').length > 0) {
+    shipObjects = JSON.parse(fs.readFileSync('plugins/ships.json'));
   }
 }
 
